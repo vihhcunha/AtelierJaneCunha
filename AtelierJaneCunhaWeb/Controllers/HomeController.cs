413ab7a52c17cb2714d0cce8288b170075433ad7
Vinicius Cunha <vinicius@hotmail.com>
2021-06-27 15:27:49 -0300

    Adicionando menu responsivo e imagens iniciais da home

--- AtelierJaneCunhaWeb/Controllers/HomeController.cs
+++ AtelierJaneCunhaWeb/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using AtelierJaneCunhaWeb.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -21,17 +20,6 @@ namespace AtelierJaneCunhaWeb.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ AtelierJaneCunhaWeb/wwwroot/js/scripts.js
@@ -0,0 +1,13 @@
+﻿const buttonToggleMenu = document.getElementById("menu-toggle-button");
+const navMenu = document.getElementById("nav-menu");
+
+buttonToggleMenu.addEventListener("click", abrirFecharMenu);
+
+function abrirFecharMenu() {
+	if (navMenu.classList.contains("nav-menu-show")) {
+		navMenu.classList.remove("nav-menu-show");
+	}
+	else {
+		navMenu.classList.add("nav-menu-show");
+	}
+}
